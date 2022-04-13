d84b4cc5e22699f81121debc5322628449c5dd64
Kristen Bartel <kristenbartel@gmail.com>
2022-04-12 19:43:23 -0500

    translator complete

--- src/Components/Translator.jsx
+++ src/Components/Translator.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import rot13 from 'rot-thirteen';
 
 class Translator extends React.Component {
     constructor(props) {
         super(props);
             this.state ={
-                text: props.initialText || '' //will have the initial text from the App.js ||/OR what ever you type in??
+                text: props.initialText || '', //will have the initial text from the App.js ||/OR what ever you type in??
+                translated: rot13(props.initialText) || ''
             }
         }
         render() {
@@ -13,14 +15,17 @@ class Translator extends React.Component {
                     <h1>Translator</h1>
                     <input type='text' onChange={(event) => { //this sets an onChange value that expects an event Object from the browser and prints the console.log() below
                         console.log(event.target.value);
+                        this._updateText(event.target.value); //updateText function is passed the input value and run through the helper func below, so the input value event.target.value is run through as newText and the updated state setState is changed to the new object text: newText
                     }}/> 
-                    <p>{this.state.text}</p>
+                    {/* below are where the above states are rendered and state changes are displayed */}
+                    <p>{this.state.text}</p> 
+                    <p>{this.state.translated}</p>
                 </div>
             );
         }
         // below is a helper method _ is used to indicate this helper
         _updateText = (newText) => {
-            this.setState({text: newText}, () => {
+            this.setState({text: newText, translated: rot13(newText)}, () => {
                 console.log(`Text updated to: ${newText}`)
             })
         }
